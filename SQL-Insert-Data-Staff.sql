db948d2da8bd38ce06294da0fdb6e5bdad8117ee
crabbbb <87376058+crabbbb@users.noreply.github.com>
2022-09-27 15:53:49 +0800

    Create SQL-Insert-Data-Staff.sql

---
+++ SQL-Insert-Data-Staff.sql
@@ -0,0 +1,2 @@
+INSERT INTO `staff` (`staff_id`, `staff_name`, `staff_email`, `staff_pass`, `staff_role`, `staff_status`, `staff_date_join`, `staff_gender`, `staff_phone_no`, `staff_address`, `staff_img`) VALUES
+(1, 'Erika Fung', 'erikafck@example.com', '123456', 'no role', 'Active', '2022-02-02', '0', '+60123456789', 'Kepong,Kuala Lumpur.', 'profilepic.jpg');
