01a620a075451d48225160d8670f64ea92269a92
crabbbb <87376058+crabbbb@users.noreply.github.com>
2022-09-27 15:52:08 +0800

    Update SQL-Insert-Data-Ticket.sql

--- SQL-Insert-Data-Ticket.sql
+++ SQL-Insert-Data-Ticket.sql
@@ -1,7 +1,7 @@
-INSERT INTO `ticket` (`ticket_id`, `exhibition_id`, `ticket_price`, `ticket_date`) VALUES
-(1000, 1000, 100, '2022-09-27 09:00:00'),
-(1001, 1003, 100, '2022-09-28 09:00:00'),
-(1002, 1001, 100, '2022-09-29 09:00:00'),
-(1003, 1002, 100, '2022-09-30 09:00:00'),
-(1004, 1004, 200, '2022-10-01 09:00:00'),
-(1005, 1004, 200, '2022-10-02 00:00:00');
+INSERT INTO `ticket` (`ticket_id`, `ticket_price`, `ticket_date`, `ticket_location`, `ticket_ppl_allow`, `exhibition_id`) VALUES
+(1000, 100, '2022-09-27 09:00:00', 'Lot 24 & 25 Publika Shopping Gallery, Jalan Dutamas 1, Kuala Lumpur', 100, 1000),
+(1001, 100, '2022-09-28 09:00:00', 'Lot 24 & 25 Publika Shopping Gallery, Jalan Dutamas 1, Kuala Lumpur', 100, 1003),
+(1002, 100, '2022-09-29 09:00:00', 'Lot 24 & 25 Publika Shopping Gallery, Jalan Dutamas 1, Kuala Lumpur', 100, 1001),
+(1003, 100, '2022-09-30 09:00:00', 'Lot 24 & 25 Publika Shopping Gallery, Jalan Dutamas 1, Kuala Lumpur', 100, 1002),
+(1004, 200, '2022-10-01 09:00:00', 'Lot 24 & 25 Publika Shopping Gallery, Jalan Dutamas 1, Kuala Lumpur', 100, 1004),
+(1005, 200, '2022-10-02 00:00:00', 'Lot 24 & 25 Publika Shopping Gallery, Jalan Dutamas 1, Kuala Lumpur', 100, 1004);
