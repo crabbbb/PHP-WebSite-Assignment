7c30a53a1335b0e3f611052ebbb601a8c54c3208
crabbbb <87376058+crabbbb@users.noreply.github.com>
2022-09-27 15:52:59 +0800

    Create SQL-Insert-Data-Exhibition.sql

---
+++ SQL-Insert-Data-Exhibition.sql
@@ -0,0 +1,6 @@
+INSERT INTO `exhibition` (`exhibition_id`, `exhibition_name`, `staff_id`) VALUES
+(1000, 'The World Famous Painting', 1000),
+(1001, 'Sketch', 1000),
+(1002, 'Abstract painting', 1000),
+(1003, 'Nature', 1000),
+(1004, 'All Paintings', 1000);
